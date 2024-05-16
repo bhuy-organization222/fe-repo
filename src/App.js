54cad6b6aeae25dda7129c26b9cbbcd260ba188d
bhuy <bhuy@infinitalk.co.jp>
2024-05-16 09:43:17 +0900

    update fe

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello Bhuy aaaaaaa
+          Hello Bhuy aaaaaaa 2222 update cache
         </a>
       </header>
     </div>
